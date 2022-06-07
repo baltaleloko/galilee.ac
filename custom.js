ae76cccd7848bb436aabbe706e25da216776b529
baltaleloko <107039572+baltaleloko@users.noreply.github.com>
2022-06-07 13:38:51 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,3 +1,3 @@
-const el = document.querySelector('i-frame');
+const el = document.querySelector('iframe');
 const observer = lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
 observer.observe();
