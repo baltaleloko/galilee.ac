f660b1f86ff46339a5ada5591c67ee998c52c57a
baltaleloko <107039572+baltaleloko@users.noreply.github.com>
2022-06-08 19:18:59 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,3 +1,5 @@
-const el = document.querySelectorALL(.filter_embedquestion-iframe);
-const observer = lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
-observer.observe();
+const boxes = document.querySelectorAll('.filter_embedquestion-iframe');
+
+for (const box of boxes) {
+  box.classList.add('lozad');
+}
